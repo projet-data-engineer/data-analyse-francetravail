83c667a8a57d4d98bae0a82a09610d249a99962a
cyril.ledean <cyril.ledean@univ-lyon3.fr>
2024-05-29 18:37:14 +0200

    Modification sur la gestion des volumes

--- airflow/dags/pipeline_offres_date.py
+++ airflow/dags/pipeline_offres_date.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 import os
+from airflow import DAG
 import pendulum
 
 from airflow.decorators import dag, task
 from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.operators.bash import BashOperator
+
 from docker.types import Mount
 
 
@@ -13,67 +16,67 @@ local_tz = pendulum.timezone("Europe/Paris")
 Tous les jours à 01h00, requêtage api des offres créées J-1, puis chargement
 """
 
-@dag(
-    dag_id="pipeline_offres_date",
-    start_date=datetime(2024, 5, 23, tzinfo=local_tz),
+dag = DAG(
+    dag_id='pipeline_offres_date',
+    description='A simple tutorial DAG',
     schedule_interval='0 1 * * *',
+    start_date=datetime(2024, 5, 23, tzinfo=local_tz),
     catchup=False
 )
-def pipeline_offres_date():
 
-    """
-    retourne <date_execution_dag> - 1 jour
-    """
-    @task
-    def date_creation(ds):
-        return (datetime.strptime(ds, '%Y-%m-%d') + timedelta(days=-1)).strftime('%Y-%m-%d')
 
-    _date_creation = date_creation()
+"""
+retourne <date_execution_dag> - 1 jour
+"""
+@task
+def date_creation(ds):
+    return (datetime.strptime(ds, '%Y-%m-%d') + timedelta(days=-1)).strftime('%Y-%m-%d')
 
-    # --mount type=bind,source="$(pwd)"/raw_data,target=/raw_data
+_date_creation = date_creation()
 
-    collecte_offres_date = DockerOperator(
+collecte_offres_date = DockerOperator(
 
-        task_id='collecte_offres_date',
-        image='collecte:latest',
-        container_name='collecte_offres_date',
-        api_version='auto',
-        auto_remove=True,
-        docker_url="TCP://docker-proxy:2375",
-        mount_tmp_dir=False,
-        command="python ./collecte_offres_date.py",
-        environment={
-            'FRANCETRAVAIL_HOST': os.getenv('FRANCETRAVAIL_HOST'),
-            'FRANCETRAVAIL_ID_CLIENT': os.getenv('FRANCETRAVAIL_ID_CLIENT'),
-            'FRANCETRAVAIL_CLE_SECRETE': os.getenv('FRANCETRAVAIL_CLE_SECRETE'),
-            'DATE_CREATION': _date_creation,
-            'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
-        },
-        mounts=[Mount(target=os.getenv('RAW_DATA_PATH'), source=os.getenv('RAW_DATA_VOLUME_NAME'), type='volume')]
-    )
+    task_id='collecte_offres_date',
+    image='collecte:latest',
+    container_name='collecte_offres_date',
+    api_version='auto',
+    auto_remove=True,
+    docker_url="TCP://docker-proxy:2375",
+    mount_tmp_dir=False,
+    command="python ./collecte_offres_date.py",
+    environment={
+        'FRANCETRAVAIL_HOST': os.getenv('FRANCETRAVAIL_HOST'),
+        'FRANCETRAVAIL_ID_CLIENT': os.getenv('FRANCETRAVAIL_ID_CLIENT'),
+        'FRANCETRAVAIL_CLE_SECRETE': os.getenv('FRANCETRAVAIL_CLE_SECRETE'),
+        'DATE_CREATION': _date_creation,
+        'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
+    },
+    mounts=[Mount(source="raw_data", target="/raw_data", type='volume')],
+    dag=dag
+)
 
-    chargement_offres_date = DockerOperator(
+chargement_offres_date = DockerOperator(
 
-        task_id='chargement_offres_date',
-        image='chargement:latest',
-        container_name='chargement_offres_date',
-        api_version='auto',
-        auto_remove=True,
-        docker_url="TCP://docker-proxy:2375",
-        mount_tmp_dir=False,
-        command="python ./chargement_offres_date.py",
-        environment={
-            'DUCKDB_FILE': os.getenv('DUCKDB_FILE'),
-            'DB_PATH': os.getenv('DB_PATH'),
-            'DATE_CREATION': _date_creation,
-            'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
-        },
-        mounts=[
-            Mount(target=os.getenv('RAW_DATA_PATH'), source=os.getenv('RAW_DATA_VOLUME_NAME'), type='volume'),
-            Mount(target=os.getenv('DB_PATH'), source=os.getenv('DB_PATH_VOLUME_NAME'), type='volume')
-        ]
-    )
+    task_id='chargement_offres_date',
+    image='chargement:latest',
+    container_name='chargement_offres_date',
+    api_version='auto',
+    auto_remove=True,
+    docker_url="TCP://docker-proxy:2375",
+    mount_tmp_dir=False,
+    command="python ./chargement_offres_date.py",
+    environment={
+        'DUCKDB_FILE': os.getenv('DUCKDB_FILE'),
+        'DB_PATH': os.getenv('DB_PATH'),
+        'DATE_CREATION': _date_creation,
+        'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
+    },
+   mounts=[
+        Mount(source="raw_data", target="/raw_data", type='volume'),
+        Mount(source="database", target="/database", type='volume')
+    ],
+    dag=dag
+)
 
-    collecte_offres_date >> chargement_offres_date
+collecte_offres_date >> chargement_offres_date
 
-pipeline_offres_date()
--- airflow/dags/pipeline_rome.py
+++ airflow/dags/pipeline_rome.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import os
+from airflow import DAG
 import pendulum
 
 from airflow.decorators import dag, task
@@ -13,54 +14,57 @@ local_tz = pendulum.timezone("Europe/Paris")
 Tous les jours à 01h00, requêtage api des offres créées J-1, puis chargement
 """
 
-@dag(
-    dag_id="pipeline_rome",
-    start_date=datetime(2024, 5, 23, tzinfo=local_tz),
+dag = DAG(
+    dag_id='pipeline_rome',
+    description='Collecte 3 niveaux nomenclature ROME depuis api francetravail.io',
     schedule=None,
+    start_date=datetime(2024, 5, 23, tzinfo=local_tz),
     catchup=False
 )
-def pipeline_offres_date():
 
-    collecte_rome = DockerOperator(
+collecte_rome = DockerOperator(
 
-        task_id='collecte_rome',
-        image='collecte:latest',
-        container_name='collecte_rome',
-        api_version='auto',
-        auto_remove=True,
-        docker_url="TCP://docker-proxy:2375",
-        mount_tmp_dir=False,
-        command="python ./collecte_rome.py",
-        environment={
-            'FRANCETRAVAIL_HOST': os.getenv('FRANCETRAVAIL_HOST'),
-            'FRANCETRAVAIL_ID_CLIENT': os.getenv('FRANCETRAVAIL_ID_CLIENT'),
-            'FRANCETRAVAIL_CLE_SECRETE': os.getenv('FRANCETRAVAIL_CLE_SECRETE'),
-            'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
-        },
-        mounts=[Mount(target=os.getenv('RAW_DATA_PATH'), source=os.getenv('RAW_DATA_VOLUME_NAME'), type='volume')]
-    )
+    task_id='collecte_rome',
+    image='collecte:latest',
+    container_name='collecte_rome',
+    api_version='auto',
+    auto_remove=True,
+    docker_url="TCP://docker-proxy:2375",
+    mount_tmp_dir=False,
+    command="python ./collecte_rome.py",
+    environment={
+        'FRANCETRAVAIL_HOST': os.getenv('FRANCETRAVAIL_HOST'),
+        'FRANCETRAVAIL_ID_CLIENT': os.getenv('FRANCETRAVAIL_ID_CLIENT'),
+        'FRANCETRAVAIL_CLE_SECRETE': os.getenv('FRANCETRAVAIL_CLE_SECRETE'),
+        'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
+    },
+    mounts=[
+        Mount(source="raw_data", target="/raw_data", type='volume')
+    ],
+    dag=dag
+)
 
-    chargement_rome = DockerOperator(
+chargement_rome = DockerOperator(
 
-        task_id='chargement_rome',
-        image='chargement:latest',
-        container_name='chargement_rome',
-        api_version='auto',
-        auto_remove=True,
-        docker_url="TCP://docker-proxy:2375",
-        mount_tmp_dir=False,
-        command="python ./chargement_rome.py",
-        environment={
-            'DUCKDB_FILE': os.getenv('DUCKDB_FILE'),
-            'DB_PATH': os.getenv('DB_PATH'),
-            'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
-        },
-        mounts=[
-            Mount(target=os.getenv('RAW_DATA_PATH'), source=os.getenv('RAW_DATA_VOLUME_NAME'), type='volume'),
-            Mount(target=os.getenv('DB_PATH'), source=os.getenv('DB_PATH_VOLUME_NAME'), type='volume')
-        ]
-    )
+    task_id='chargement_rome',
+    image='chargement:latest',
+    container_name='chargement_rome',
+    api_version='auto',
+    auto_remove=True,
+    docker_url="TCP://docker-proxy:2375",
+    mount_tmp_dir=False,
+    command="python ./chargement_rome.py",
+    environment={
+        'DUCKDB_FILE': os.getenv('DUCKDB_FILE'),
+        'DB_PATH': os.getenv('DB_PATH'),
+        'RAW_DATA_PATH': os.getenv('RAW_DATA_PATH')
+    },
+    mounts=[
+        Mount(source="raw_data", target="/raw_data", type='volume'),
+        Mount(source="database", target="/database", type='volume')
+    ],
+    dag=dag
+)
 
-    collecte_rome >> chargement_rome
+collecte_rome >> chargement_rome
 
-pipeline_offres_date()
